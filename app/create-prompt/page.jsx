3397817d9d1a819c8aa5ea279ce5b236a2514a7e
ValarMorghulisHenSyndror <alexandrtcacenco324@gmail.com>
2023-07-26 21:45:37 +0300

    The Feed Finished, The Delete And Edit Options Added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,7 +6,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const CreatePrompt = () => {
-    
+
     const router = useRouter();
     const { data: session } = useSession(); 
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client"
+
+import React, {useEffect, useState}  from 'react';
+import PromptCardList from './PromptCardList';
 
 function Feed() {
+    
+    const [searchText, setSearchText] = useState("");
+    const [posts, setPosts] = useState([]);
+    
+    const handleSearchChange = (e) => {
+        
+    };
+    
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch("/api/prompt");
+            const data = await response.json();
+            setPosts(data);
+        };
+        console.log("These are the posts ", posts);
+        fetchPosts();
+    }, []);
+
     return (
-        <>Feed</>
+        <section className='feed'>
+            <form className="relative w-full flex-center">
+                <input type="text" placeholder='Search for a tag or a username' value={searchText} onChange={handleSearchChange} required className='search_input peer'/>
+            </form>
+            <PromptCardList data={posts} handleTagClick={()=>{}}/>
+        </section>
     );
 }
 
